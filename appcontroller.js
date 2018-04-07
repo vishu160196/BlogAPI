bd04ec15dbd6f090aceea4533ff8d93d4c176ff8
vishal <vishu160196@gmail.com>
2018-04-07 21:19:30 +0530

    create feed endpoint

--- appcontroller.js
+++ appcontroller.js
@@ -3,7 +3,42 @@ var url=require('./server').url
 var models = require('./models')
 
 exports.follow = function(req, res){
-    
+    var followed = req.params.username
+    var username=req.user.username
+    if(followed.length===0){
+        res.statusMessage='missing field values'
+        res.status(500).send(JSON.stringify({error: 'one or more field values missing'}))
+        return
+    }
+
+    mongoclient.connect(url, {uri_decode_auth : true}, function(err, db){
+        if(!err){
+            db=db.db('user')
+            db.collection('users').findOne({username : username}, {following : true}, function(err, doc){
+                if(doc.following.includes(followed)){
+                    res.statusMessage='No content'
+                    res.status(204).send(JSON.stringify({message: `you are already following ${followed}`}))
+                    
+                }
+                else{
+                    doc.following.push(followed)
+                    db.collection('users').findOneAndUpdate({username : username}, {$set : {following : doc.following}}, function(err, doc){
+                        if(!err){
+                            res.statusMessage='No content'
+                            res.status(204).send(JSON.stringify({message: `you are now following ${followed}`}))
+                        }
+                        else{
+                            res.statusMessage='Internal server error'
+                            res.status(500).send(JSON.stringify({error: err.toString()}))
+                        }
+                    })
+                }
+            })
+        }else{
+            res.statusMessage='Internal server error'
+            res.status(500).send(JSON.stringify({error: 'Unable to connect to database'}))
+        }
+    })
 }
 
 exports.publishPost = function(req, res){
@@ -35,12 +70,46 @@ exports.publishPost = function(req, res){
         }else{
             res.statusMessage='Internal server error'
             res.status(500).send(JSON.stringify({error: 'Unable to connect to database'}))
-        }
-        
-        
+        } 
     })
 }
 
 exports.getFeed = function(req, res){
-    
+    var username=req.user.username
+    mongoclient.connect(url, {uri_decode_auth : true}, function(err, db){
+        if(!err){
+            db=db.db('user')
+            db.collection('users').findOne({username : username}, {following : true}, function(err, doc){
+            
+                if(!err){
+                    db.collection('users').find({username: {$in : doc.following}}, {username : true, blogs: true}, function(err, docs){
+                        if(!err){
+                            docs.toArray(function(err, items){
+                                feeds=[]
+                                items.forEach(element => {
+                                    feed={
+                                        username: element.username,
+                                        blogs: element.blogs
+                                    }
+                                    feeds.push(feed)
+                                });
+                                res.statusMessage='OK'
+                                res.status(200).send(JSON.stringify({feeds}))
+                            })
+                        }else{
+                            res.statusMessage='Internal server error'
+                            res.status(500).send(JSON.stringify({error: err.toString()}))
+                        }
+                    
+                    })
+                }else{
+                    res.statusMessage='Internal server error'
+                    res.status(500).send(JSON.stringify({error: err.toString()}))
+                }
+            })
+        }else{
+            res.statusMessage='Internal server error'
+            res.status(500).send(JSON.stringify({error: 'Unable to connect to database'}))
+        }
+    })
 }
